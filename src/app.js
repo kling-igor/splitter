ad8a24ee4b5731451a0646ae1c1bacfc873e73cb
Igor Kling <klingiv@altarix.ru>
2019-02-22 15:05:37 +0400

    горизонтальный сплит

--- src/app.js
+++ src/app.js
@@ -1,10 +1,8 @@
-import React, { PureComponent } from "react"
+import React, { PureComponent } from "react";
 
-import styled, { createGlobalStyle } from "styled-components"
+import styled, { createGlobalStyle } from "styled-components";
 
-import Split from 'react-split'
-
-
+import Split from "react-split";
 
 const GlobalStyle = createGlobalStyle`
   html {
@@ -39,6 +37,8 @@ const GlobalStyle = createGlobalStyle`
   .split {
     overflow-y: auto;
     overflow-x: hidden;
+    display: flex;
+    flex-direction: row;
   }
 
   .gutter {
@@ -61,46 +61,55 @@ const GlobalStyle = createGlobalStyle`
   .gutter.gutter-horizontal {
     background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAeCAYAAADkftS9AAAAIklEQVQoU2M4c+bMfxAGAgYYmwGrIIiDjrELjpo5aiZeMwF+yNnOs5KSvgAAAABJRU5ErkJggg==');
     cursor: col-resize;
+    height: 100%;
+    float: left;
   }
 
   .split, .gutter.gutter-horizontal {
-      float: left;
       height: 300px;
   }
 `;
 
-const List = () => {
-
-  const children = []
+const List = ({ color }) => {
+  const children = [];
 
   for (let i = 0; i < 50; i += 1) {
-    const key = i
-    children.push(<li key={key}>{`item ${i}`}</li>)
+    const key = i;
+    children.push(<li key={key}>{`item ${i}`}</li>);
   }
 
-  // return (
-  //   <ol style={{ background: 'yellow' }}>
-  //     {children}
-  //   </ol>
-  // )
+  return (
+    <ol
+      style={{
+        background: color,
+        width: 600,
+        height: 300,
+        overflow: "auto",
+        margin: 0,
+        padding: 0
+      }}
+    >
+      {children}
+    </ol>
+  );
 
-  return <div style={{ background: 'green', width: '300px', height: '300px' }}></div>
-}
+  // return <div style={{ background: color, width: 600, height: 300 }} />;
+};
 
 function elementStyle(dimension, size, gutterSize) {
   return {
-    // 'flex-basis': 'calc(' + size + '% - ' + gutterSize + 'px)',
-    // float: 'right'
-  }
+    "flex-basis": "calc(" + size + "% - " + gutterSize + "px)",
+    float: "right",
+    width: size
+  };
 }
 
 function gutterStyle(dimension, gutterSize) {
   return {
-    // float: 'right',
-    // width: '10px'
-  }
+    float: "right",
+    width: "10px"
+  };
 }
-
 
 export default class App extends PureComponent {
   render() {
@@ -116,17 +125,25 @@ export default class App extends PureComponent {
           </div>
           <div style={{ width: '100%', height: '100%', background: 'green' }} />
         </Split> */}
-        <Split sizes={[25, 75]} minSize={[100, 100]} direction="horizontal" elementStyle={elementStyle} gutterStyle={gutterStyle}>
-          <List />
-          <List />
+        <Split
+          style={{ width: "100%", height: 300, display: "flex" }}
+          sizes={[25, 75]}
+          minSize={[100, 100]}
+          direction="horizontal"
+          elementStyle={elementStyle}
+          gutterStyle={gutterStyle}
+        >
+          <List color="orange" />
+          <List color="yellow" />
         </Split>
       </>
-    )
+    );
   }
 }
 
-
-{/* <Split style={{ height: '100%', width: '300px', overflow: 'auto' }} sizes={[50, 50]} minSize={[100, 100]} direction="vertical">
+{
+  /* <Split style={{ height: '100%', width: '300px', overflow: 'auto' }} sizes={[50, 50]} minSize={[100, 100]} direction="vertical">
 <List />
 <List />
-</Split> */}
+</Split> */
+}
